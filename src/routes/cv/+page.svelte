1d48c128596fa10063ecb0db4a06c0f866648209
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2025-03-23 17:43:46 +0100

    update

---
+++ src/routes/cv/+page.svelte
@@ -0,0 +1,72 @@
+
+<div class="w-full grid grid-cols-1 gap-4">
+
+    <!-- Headline -->
+    <div class="grid grid-cols-2 p-4 gap-4">
+        <div>
+            <div>JACOB ROBERTS</div>
+            <div>Product Manager | Pass Expertise | Strategic Planning</div>
+            <!-- Contact information -->
+             <div class="grid grid-cols-2">
+                <div>+41787388282</div>
+                <div>test@getDefaultNormalizer.cpm</div>
+                <div>linkedin.com</div>
+                <div>1234 Main St, City, State, 12345</div>
+            </div>
+        </div>
+        <div>
+            PHOTO
+        </div>
+    </div>
+
+    <!-- Main Content -->
+    <div class="grid grid-cols-2 gap-4 p-4">
+        <!-- Left Column -->
+        <div>
+            <div class="flex flex-col gap-4">
+                <!-- Summary -->
+                <div>
+                    <div class="text-lg">Summary</div>
+                    <div>Product Manager with 10 years of experience in the technology sector. Proven track record of success in launching new products and increasing sales. Skilled in strategic planning and industry analysis. Excellent communication and leadership abilities.</div>
+                </div>
+
+                <!-- Experience -->
+                <div>
+                    <div class="text-lg">Experience</div>
+                    <div>
+                        <div>Seniour Product Manager</div>
+                        <!-- Dates -->
+                        <div>
+                            <div class="text-sm text-cyan-800">TechSolution Ltd.</div>
+                            <div class="flex flex-row">
+                                <div class="min-w-[100]">07/2019 - Present</div>
+                                <div>London, UK</div>
+                            </div>
+                            <ol>
+                                <li>Developed and launched new product line that increased sales by 20%.</li>
+                                <li>Managed cross-functional teams to ensure product development met deadlines.</li>
+                                <li>Conducted market research to identify new opportunities for growth.</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Education -->
+                <div>
+                    <div class="text-lg">Education</div>
+                    <div>
+                        <div>Master of Science in Product management</div>
+                        <div>University of Edinbourg</div>
+                        <div class="flex flex-row">
+                            <div class="min-w-[100]">07/2019 - 08/2020</div>
+                            <div>London, UK</div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+  
+	
+</div>
